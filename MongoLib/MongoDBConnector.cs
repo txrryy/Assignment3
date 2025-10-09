4e7d74f9210cbeb9f7608e6de3fdb529e99a8b7d
Terry <talb5624@students.vsu.edu>
2025-10-08 22:34:35 -0400

    add MongoDBConnector constructor

--- MongoLib/MongoDBConnector.cs
+++ MongoLib/MongoDBConnector.cs
@@ -1,6 +1,14 @@
-﻿namespace MongoLib;
+﻿using MongoDB.Driver;
+
+namespace MongoLib;
 
 public class MongoDBConnector
 {
+    private readonly MongoClient _client;
 
+
+    public MongoDBConnector(string connectionString)
+    {
+        _client = new MongoClient(connectionString);
+    }
 }
